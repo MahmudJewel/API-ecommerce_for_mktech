477fb07f290f566f186ad20819f21fd05e3b6cc8
villain <jmbengal.mbstu@gmail.com>
2022-09-07 21:30:32 +0600

    product model added

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # installed app 
+    'api',
+    'customer',
+    'product',
+
+    # third party app 
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +124,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# for static and media files 
+#start Added by me
+STATICFILES_DIRS = [ #Search during collectstatic
+os.path.join(BASE_DIR,'static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'djangoAutoStatic') #collectstatic==> automatically created 'djangoStatic' folder.
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media') #for imagefield
+MEDIA_URL = "/media/" #it will help to access static directory through browser
+#End added
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- product/models.py
+++ product/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class product(models.Model):
+	name =models.CharField(max_length=50)
+	stock = models.PositiveIntegerField()
+	price = models.PositiveIntegerField()
+	img1 =models.ImageField(default="product/default.png", upload_to="product/", null=True, blank=True)
+	img2 =models.ImageField(default="product/default.png", upload_to="product/", null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+
