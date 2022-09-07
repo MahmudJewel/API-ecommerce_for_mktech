33b645172cd891f81f0e255f3430e7ccfeaddd37
villain <jmbengal.mbstu@gmail.com>
2022-09-08 00:50:32 +0600

    permissions added

--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
 
 from .serializers import ProductSerializers, UserSerializers, OrderSerializers
 from product.models import Product
@@ -9,15 +10,37 @@ from customer.models import Order
 
 # product viewset
 class ProductViewset(viewsets.ModelViewSet):
-    serializer_class = ProductSerializers
-    queryset = Product.objects.all()
+	serializer_class = ProductSerializers
+	queryset = Product.objects.all()
+
+	def get_permissions(self):
+		if self.request.method == 'GET':
+			self.permission_classes = [IsAuthenticatedOrReadOnly, ]
+		else:
+			self.permission_classes = [IsAdminUser, ]
+		return super(ProductViewset, self).get_permissions()
+
 
 # user viewset 
 class UserViewset(viewsets.ModelViewSet):
-    serializer_class = UserSerializers
-    queryset = User.objects.all()
+	serializer_class = UserSerializers
+	queryset = User.objects.all()
+
+	def get_permissions(self):
+		if self.request.method == 'POST':
+			self.permission_classes = [AllowAny, ]
+		else:
+			self.permission_classes = [IsAdminUser, ]
+		return super(UserViewset, self).get_permissions()
 
 # order viewset 
 class OrderViewset(viewsets.ModelViewSet):
-    serializer_class = OrderSerializers
-    queryset = Order.objects.all()
+	serializer_class = OrderSerializers
+	queryset = Order.objects.all()
+
+	def get_permissions(self):
+		if self.request.method == 'POST':
+			self.permission_classes = [IsAuthenticated, ]
+		else:
+			self.permission_classes = [IsAdminUser, ]
+		return super(OrderViewset, self).get_permissions()
